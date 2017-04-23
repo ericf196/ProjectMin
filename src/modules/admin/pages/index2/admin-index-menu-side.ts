4e498278b54fa9b417cdf2baca69bf23cce1eeab
Eric Freitez <ericf196@gmail.com>
2017-04-23 13:00:09 -0400

    v1 menuSide

--- src/modules/admin/pages/index2/admin-index-menu-side.ts
+++ src/modules/admin/pages/index2/admin-index-menu-side.ts
@@ -1,22 +1,49 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
 
-/*
-  Generated class for the AdminIndexMenuSide page.
+//import { TabsPage } from '../pages/tabs/tabs';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+import { SettingsPage } from '../pages/settings/settings';
+import { AccountPage } from '../pages/account/account';
+
+import {AdminDashboardPage} from "../dashboard/admin-dashboard";
+
 @Component({
   selector: 'page-admin-index-menu-side',
   templateUrl: 'admin-index-menu-side.html'
 })
 export class AdminIndexMenuSidePage {
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  rootPage = AdminDashboardPage;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AdminIndexMenuSidePage');
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Homepage', component: AdminDashboardPage },
+      { title: 'Settings', component: AdminDashboardPage },
+      { title: 'Account', component: AdminDashboardPage }
+  	];
+
+ 
+  }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+    });
+  }
 }
